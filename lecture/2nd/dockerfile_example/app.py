85e435cc60f0f2e4d3fe6e86e2cdf00a4519a8d3
Rachel <cofla226@naver.com>
2024-05-13 23:33:27 +0900

    Update: Modify salary wish in hello_world function

--- lecture/2nd/dockerfile_example/app.py
+++ lecture/2nd/dockerfile_example/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '수강생들의 빠른 취업, 연봉 상승 기원합니다!'
+    return '수강생들의 빠른 취업, 빠른 연봉 1억 상승 기원합니다!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
